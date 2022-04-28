80e0c20d037eab8efffd3d9ad3a067be0565acc1
SpaceXElaborator <terturlminecraft@gmail.com>
2022-04-28 15:44:21 -0700

    Removed all SpringBoot dependencies but main starter. Added starter Main class to run SpringBoot

---
+++ src/main/java/com/srahman/ArmyTracker/Tracker.java
@@ -0,0 +1,13 @@
+package com.srahman.ArmyTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tracker {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tracker.class, args);
+	}
+	
+}
